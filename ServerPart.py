8e520c8f300d8a06a9a7439378feed261ce61e1f
ACHRAF <achrafsaadalii@gmail.com>
2025-11-18 18:44:05 +0100

    neww

--- ServerPart.py
+++ ServerPart.py
@@ -304,8 +304,8 @@ app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
 
 
 
-
+''''
 if __name__ == '__main__':
 
-    app.run(debug = True , host = '0.0.0.0' , port = 5000)
+    app.run(debug = True , host = '0.0.0.0' , port = 5000)'''
     
